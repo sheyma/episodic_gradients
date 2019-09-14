c216a468661d8a9613a2f6dc27f71f0867f0556c
Şeyma Bayrak <sheymaba@gmail.com>
2019-09-14 17:09:43 +0200

    get subject TMS intensity

---
+++ neuronav/tms_01_intensity.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file_in  = '/data/pt_neuam005/simnibs_TMS/Participants_data_intake_v2.xlsx'
+file_out = '/data/pt_neuam005/simnibs_TMS/tms_estimate.xlsx'
+outpath  = '/data/pt_neuam005/simnibs_TMS/subjects/'
+db       = pd.read_excel(file_in, index_col='VP_Number')
+
+### HARD CODED FROM TMS MASCHINE
+x = np.array([10, 20, 30, 40, 50, 50, 70, 80, 90, 100])
+y = np.array([13, 27, 44, 59, 75, 90, 106, 121, 137, 153])
+
+### LINEAR FIT
+a, b, r_value, p_value, std_err = stats.linregress(x, y)
+y_fit  = a * x + b
+
+### FIT SUBJECT DATA
+myindex    = db.index.dropna()
+x_subjects = (np.array(db.loc[myindex]['RTMS_intensity'].values))
+y_estimate = a * x_subjects + b
+
+### PLOT
+fig = plt.figure()
+line1 = plt.plot(x,y, 'o', label='coil')
+line2 = plt.plot(x, y_fit, label= 'coil fit')
+line3 = plt.plot(x_subjects, y_estimate, 'or', label='subjects')
+plt.legend()
+plt.xlabel('Instensity Percentage')
+plt.ylabel('Intensity [A/$\mu$ s] ')
+#plt.show()
+
+# GENERATE NEW DATA FRAME
+rColumns = ['subject_id', 'TMS_est', 'RTMS_int', ]
+df       = pd.DataFrame(index=myindex,
+                        columns=rColumns)
+
+# SAVE VALUES INDIVIDUALLY FOR EACH SUBJECT
+for idx in myindex:
+    df.loc[idx]['RTMS_int'] = db.loc[idx]['RTMS_intensity']
+    df.loc[idx]['TMS_est']  = a * (db.loc[idx]['RTMS_intensity']) + b
+
+    if idx < 10:
+        df.loc[idx]['subject_id'] = 'sub-0' + str(int(idx))
+
+        if os.path.exists(outpath + 'sub-0' + str(int(idx)))==True:
+            print('sub-0' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
+            np.savetxt(outpath + 'sub-0' + str(int(idx)) +
+                       '/tms_int_sub-0' + str(int(idx)) + '.txt',
+                       np.array([df.loc[idx]['TMS_est']]), fmt='%2.2f')
+    else:
+        df.loc[idx]['subject_id'] = 'sub-' + str(int(idx))
+
+        if os.path.exists(outpath + 'sub-' + str(int(idx)))==True:
+            print('sub-' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
+            np.savetxt(outpath + 'sub-' + str(int(idx)) +
+                        '/tms_int_sub-' + str(int(idx)) + '.txt',
+                        np.array([df.loc[idx]['TMS_est']]), fmt='%2.2f')
+
+#df.to_excel(file_out)
