5abb0d5c44fd8fe06c385b50fd958baf1a3196e4
Şeyma Bayrak <sheymaba@gmail.com>
2019-09-16 19:25:10 +0200

    coil handling direction saved for each subject

--- neuronav/tms_01_intensity.py
+++ neuronav/tms_01_intensity.py
@@ -33,30 +33,49 @@ plt.ylabel('Intensity [A/$\mu$ s] ')
 #plt.show()
 
 # GENERATE NEW DATA FRAME
-rColumns = ['subject_id', 'TMS_est', 'RTMS_int', ]
+rColumns = ['subject_id', 'TMS_est', 'RTMS_int', 'mpfc_direc']
 df       = pd.DataFrame(index=myindex,
                         columns=rColumns)
 
 # SAVE VALUES INDIVIDUALLY FOR EACH SUBJECT
 for idx in myindex:
+    # get RTMS_intensity and fit it to linear regression
     df.loc[idx]['RTMS_int'] = db.loc[idx]['RTMS_intensity']
     df.loc[idx]['TMS_est']  = a * (db.loc[idx]['RTMS_intensity']) + b
 
+    # get mPFC stimulation coil direction
+    if db.loc[idx]['Stimulation_01'] == 'mPFC':
+        df.loc[idx]['mpfc_direc'] = db.loc[idx]['Handle_Dir.1']
+    if db.loc[idx]['Stimulation_02'] == 'mPFC':
+        df.loc[idx]['mpfc_direc'] = db.loc[idx]['Handle_Dir.2']
+    if db.loc[idx]['Stimulation_03'] == 'mPFC':
+        df.loc[idx]['mpfc_direc'] = db.loc[idx]['Handle_Dir.3']
+
+    # save...
     if idx < 10:
         df.loc[idx]['subject_id'] = 'sub-0' + str(int(idx))
 
         if os.path.exists(outpath + 'sub-0' + str(int(idx)))==True:
-            print('sub-0' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
+            #print('sub-0' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
             np.savetxt(outpath + 'sub-0' + str(int(idx)) +
                        '/tms_int_sub-0' + str(int(idx)) + '.txt',
                        np.array([df.loc[idx]['TMS_est']]), fmt='%2.2f')
+
+            np.savetxt(outpath + 'sub-0' + str(int(idx)) +
+                       '/mpfc_dir_sub-0' + str(int(idx)) + '.txt',
+                       np.array([df.loc[idx]['mpfc_direc']]), fmt='%s')
+
     else:
         df.loc[idx]['subject_id'] = 'sub-' + str(int(idx))
 
         if os.path.exists(outpath + 'sub-' + str(int(idx)))==True:
-            print('sub-' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
+            #print('sub-' + str(int(idx)), np.array([df.loc[idx]['TMS_est']]))
             np.savetxt(outpath + 'sub-' + str(int(idx)) +
                         '/tms_int_sub-' + str(int(idx)) + '.txt',
                         np.array([df.loc[idx]['TMS_est']]), fmt='%2.2f')
+
+            np.savetxt(outpath + 'sub-' + str(int(idx)) +
+                       '/mpfc_dir_sub-' + str(int(idx)) + '.txt',
+                       np.array([df.loc[idx]['mpfc_direc']]), fmt='%s')
 
 #df.to_excel(file_out)
