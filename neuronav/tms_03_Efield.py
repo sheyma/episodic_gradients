2a0454e3ea48c6d665e44899045518f11df50f90
Şeyma Bayrak <sheymaba@gmail.com>
2019-09-16 19:21:57 +0200

    coil handling direction read in

--- neuronav/tms_03_Efield.py
+++ neuronav/tms_03_Efield.py
@@ -1,5 +1,5 @@
 # run by using the following python interpreter:
-# $ simnibs_python test.py
+# $ simnibs_python tms_03_Efield.py sub-01
 # simnibs_python comes with SimNIBS installation
 
 import os, sys
@@ -18,9 +18,18 @@ m2mfile   = os.path.join(sub_dir, sub, 'm2m_' + sub)
 outfile   = os.path.join(sub_dir, sub, 'simnibs_simulation')
 intFile   = os.path.join(sub_dir, sub, 'tms_int_' + sub + '.txt' )
 coordFile = os.path.join(sub_dir, sub, 'coordinates_' + sub + '.txt' )
+handlFile = os.path.join(sub_dir, sub, 'mpfc_dir_' + sub + '.txt' )
 
 tint  = np.loadtxt(intFile).astype(float)
 coord = np.loadtxt(coordFile).astype(float)[1:].tolist()[0]
+direc = np.loadtxt(handlFile, dtype='str')
+
+if direc == 'right':
+    direction = [1,0,0]
+elif direc == 'left':
+    direction = [-1,0,0]
+
+print(sub, direc, direction)
 
 # Initalize a session
 s = sim_struct.SESSION()
@@ -35,19 +44,21 @@ tmslist                 = s.add_tmslist()
 tmslist.fnamecoil       =  coilnifti
 tmslist.anisotropy_type = 'scalar'
 
-# Initialize a TEMPORARY coil position to get params
+# Initialize coil params
 pos = tmslist.add_position()
 
 pos.centre   = coord	             # entry coordinates
 pos.distance = 1.                    # coil distance (mm)
 pos.didt     = tint * 1e6            # TMS intensity (A/s)
-pos.pos_ydir = ([1,0,0])             # coil towards right
+pos.pos_ydir = (direction)           # coil to the right or left
 pos.calc_matsimnibs(mesh)            # scalp projection
-pos.pos_ydir = (pos.matsimnibs[0:3,3] + [1,0,0]).tolist()
+pos.pos_ydir   = (pos.matsimnibs[0:3,3] + direction).tolist()
 pos.matsimnibs = None                # matsimnibs is not recomputed if already there.
 
-k.open_in_gmsh = False
-k.map_to_vol   = True
-k.map_to_MNI   = True
+# here we go...
+s.open_in_gmsh = False
+s.map_to_fsavg = True
+s.map_to_vol   = True
+s.map_to_MNI   = True
 
-run_simnibs(k)
+run_simnibs(s)
