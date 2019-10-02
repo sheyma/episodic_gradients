5d19f81a97c8fa4c48b404407ba56aa5b2b0a996
Şeyma Bayrak <sheymaba@gmail.com>
2019-10-02 18:10:25 +0200

    fisher r2z added

--- gradients/g01_gradients_scipy.py
+++ gradients/g01_gradients_scipy.py
@@ -5,9 +5,9 @@ import numexpr as ne
 from mapalign import embed
 import argparse
 
-subj_file = 'x_preprocessed.nii.gz'
-img_mask  = '/data/pt_neuam005/sheyma/grouplevel/rest_intra_mask_mni_gm_060.nii.gz'
-out_prfx  = '/data/pt_neuam005/sheyma/out/x_preprocessed'
+#subj_file = 'x_preprocessed.nii.gz'
+#img_mask  = '/data/pt_neuam005/sheyma/grouplevel/rest_intra_mask_mni_gm_060.nii.gz'
+#out_prfx  = '/data/pt_neuam005/sheyma/out/x_preprocessed'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-l','--in', dest='sbj_file')
@@ -41,16 +41,14 @@ if voxel_zeros > 0:
 
 print('calculating correlation matrix...')
 indiv_matrix = np.corrcoef(t_series)
+indiv_matrix[np.where(indiv_matrix < -0.99)] = -0.99
+indiv_matrix[np.where(indiv_matrix > 0.99)] = 0.99
 print(img_rest, indiv_matrix.shape)
 
-def fisher_z2r(Z):
-    X = ne.evaluate('exp(2*Z)')
-    return ne.evaluate('(X - 1) / (X + 1)')
+print("Fisher r2z transform...")
+indiv_matrix = np.arctanh(indiv_matrix)
 
-print("Fisher z2r transform...")
-indiv_matrix = fisher_z2r(indiv_matrix)
-
-#### Step 2 #### threhold at 90th percentile
+#### Step 2 #### threshold at 90th percentile
 print('thresholding each row at its 90th percentile...')
 perc = np.array([np.percentile(x, 90) for x in indiv_matrix])
 N    = indiv_matrix.shape[0]
