c17719b33fa89a812d6057fe2b2c4b710af1fe1d
Şeyma Bayrak <sheymaba@gmail.com>
2019-10-02 21:52:51 +0200

    average corr and get gradient

--- gradients/g03_corr_group.py
+++ gradients/g03_corr_group.py
@@ -8,7 +8,7 @@ import argparse
 
 #img_mask  = '/data/pt_neuam005/sheyma/grouplevel/rest_intra_mask_mni_gm_060.nii.gz'
 #subj_list = ['x_preprocessed.nii.gz', 'y_preprocessed.nii.gz', ...]
-#out_file  = '/data/pt_neuam005/sheyma/gradients_group/corr_mean_ses-XX-YYYY.h5'
+#out_file  = '/data/pt_neuam005/sheyma/gradients_group/ave_ses-01_past'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-l','--list', dest='subject_list',
@@ -20,9 +20,6 @@ results  = parser.parse_args()
 img_mask = results.mask_name
 imlist   = results.subject_list
 out_file = results.out_name
-
-def fisher_r2z(R):
-    return ne.evaluate('arctanh(R)')
 
 def mask_check(rest, mask):
     """
@@ -57,13 +54,44 @@ for img_rest in imlist:
     i += 1
 
 # get the mean
-SUM = ne.evaluate('SUM / N')
+indiv_matrix = ne.evaluate('SUM / N')
+print('average matrix: ', indiv_matrix.shape, indiv_matrix.min(), indiv_matrix.max())
 
-print(np.shape(SUM))
-print(SUM.min(), SUM.max())
+indiv_matrix[np.where(indiv_matrix < -0.99)] = -0.99
+indiv_matrix[np.where(indiv_matrix > 0.99)] = 0.99
 
-h   = h5py.File(out_file, 'w')
-h.create_dataset("data", data=SUM)
-h.close()
+print("Fisher r2z transform...")
+indiv_matrix = np.arctanh(indiv_matrix)
+print('fisher matrix: ', indiv_matrix.shape, indiv_matrix.min(), indiv_matrix.max())
 
+#### Step 2 #### threshold at 90th percentile
+print('thresholding each row at its 90th percentile...')
+perc = np.array([np.percentile(x, 90) for x in indiv_matrix])
+N    = indiv_matrix.shape[0]
 
+for i in range(N):
+    indiv_matrix[i, indiv_matrix[i,:] < perc[i]] = 0
+
+#neg_values = np.array([sum(indiv_matrix[i,:] < 0) for i in range(N)])
+#print('Negative values occur in %d rows' % sum(neg_values > 0))
+indiv_matrix[indiv_matrix < 0] = 0
+
+#### Step 3 #### compute the affinity matrix
+print('calculating affinity matrix with numpy linalg ...')
+NORM         = (1.0 / np.linalg.norm(indiv_matrix, axis=0).reshape([N,1]))
+indiv_matrix = np.dot(indiv_matrix,indiv_matrix) * NORM * NORM.T
+print('affinity shape ', np.shape(indiv_matrix))
+
+#### Step 4 #### get gradients
+print('computing gradients...')
+# NOTE this is fast but uses a lot of memory. So if we would save the matrix
+# here, then we could run everything above more parallel above all subjects
+emb, res = embed.compute_diffusion_map(indiv_matrix, alpha = 0.5,
+                                       n_components = 10,
+                                       return_result=True)
+
+out_name_emb = out_file + '_dense_emb.npy'
+out_name_res = out_file + '_dense_res.npy'
+print(out_name_emb)
+np.save(out_name_emb, emb)
+np.save(out_name_res, res['lambdas'])
