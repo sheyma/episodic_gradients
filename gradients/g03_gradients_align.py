9369b4154fea8490a553dec1568cc640405823c5
Şeyma Bayrak <sheymaba@gmail.com>
2019-10-08 15:12:35 +0200

    add procrustes alignment for individual subjects

--- gradients/g03_gradients_align.py
+++ gradients/g03_gradients_align.py
@@ -23,52 +23,54 @@ results = parser.parse_args()
 im_list = results.file_list
 gm_mask = results.mask_name
 mni_tmp = results.mni_name
-out_pat = results.out_path
+outdir  = results.out_path
 targ_fi = results.target
 
-# load each embedding as numpy array and concatenate all 
-alist = []
-for im in im_list:
-    im_load = np.load(im)
-    alist.append(im_load.T)
-
-# stack embedding arrays 
-embeddings = list(np.dstack(alist).T)
-print("list of all embeddings: ", np.shape(embeddings))  #### (1, 73472, 10)
-
-# get the target embedding
-target = np.load(targ_fi)
-print("shape of target embeddings: ", np.shape(target))  ### (73472, 10)
-
-# run the alignment
-# realigned, xfsm = align.iterative_alignment(embeddings, n_iters=10) >>> SATRA
-
-realigned, mean_dataset = alignment.procrustes_alignment(data=embeddings,
-                                                         reference=target,
-                                                         n_iter=10)  # >>> brainspace
-
-print("shape after realignment: ", np.shape(realigned))  #### (1, 73472, 10)
-
-# upload mni template 
+# upload mni template
 mni_affine = nb.load(mni_tmp).get_affine()
-
-# get voxel indices (x,y,z) of gray matter mask = 1
+# get voxel indices (x,y,z) from gray matter mask
 mask_array = nb.load(gm_mask).get_data()
-voxel_x = np.where(mask_array == 1)[0]
-voxel_y = np.where(mask_array == 1)[1]
-voxel_z = np.where(mask_array == 1)[2]
-print("%s voxels are in GM..." % len(voxel_x))
+x, y, z    = np.where(mask_array==1)
+print("%s voxels are in GM..." % len(x))
 
-# project aligned arrays back to the mni template space and save as nifti
-for im, realign in zip(im_list, realigned):
+# define function to save numpy arrays as nifti
+def save_niis(bname, k, A):
+    aname        = 'align_' + str(k + 1) + '_' + bname + '.nii.gz'
+    fname        = os.path.join(outdir, aname)
+    tmp          = np.zeros(nb.load(mni_tmp).get_data().shape)
+    tmp[x, y, z] = A
+    tmp_img      = nb.Nifti1Image(tmp, mni_affine)
+    nb.save(tmp_img, fname)
+    return fname
 
-    for i in range(0, 10):
-        aname = 'align_' + str(i + 1) + '_' + os.path.basename(im)[:-4] + '.nii.gz'
-        fname = os.path.join(out_pat, aname)
-        print(im, i, np.shape(realign), fname)
+# get the target embedding as numpy array
+reference = np.load(targ_fi)
 
-        tmp = np.zeros(nb.load(mni_tmp).get_data().shape)
-        tmp[voxel_x, voxel_y, voxel_z] = realign[:, i]
-        tmp_img = nb.Nifti1Image(tmp, mni_affine)
+# get the embeddings as a numpy array or a list
+if len(im_list) == 1:
+    embeddings = np.load(im_list[0]) ### (73472, 5)
+    realigned  = alignment.procrustes(source=embeddings,
+                                      target=reference) ### (73472, 5)
 
-        nb.save(tmp_img, fname)
+    for i in range(0, np.shape(realigned)[1]):
+        fname = save_niis(os.path.basename(im_list[0])[:-4], i, realigned[:, i])
+        print(i,  realigned[:, i].min(), realigned[:, i].max(), '\n', fname)
+
+else:
+    alist = []
+    for im in im_list:
+        im_load = np.load(im)
+        alist.append(im_load.T)
+    embeddings = list(np.dstack(alist).T) ### (2, 73472, 5)
+    realigned, M = alignment.procrustes_alignment(data=embeddings,
+                                                  reference=reference,
+                                                  n_iter=10) ### (2, 73472, 5)
+
+    #reference = list(np.dstack(reference.T).T.T)
+    #ALL = np.concatenate((reference, embeddings), axis=0) ### (3, 73472, 5)
+    #realigned, xfsm = align.iterative_alignment(ALL, n_iters=10) ### SATRA
+
+    for im, realign in zip(im_list, realigned):
+        for i in range(0, np.shape(realign)[1]):
+            fname = save_niis(os.path.basename(im)[:-4], i, realign[:, i])
+            print(i, realign[:,i].shape, realign[:, i].min(), realign[:, i].max(), '\n', fname)
